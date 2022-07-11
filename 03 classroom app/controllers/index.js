61ea45b8f4d8417954c5a74c867b643fc2f1be67
Dino Imeri <dinoimeri@gmail.com>
2022-07-11 21:30:27 +0200

    classroom app

---
+++ 03 classroom app/controllers/index.js
@@ -0,0 +1,63 @@
+const users = [{id: 1, name: 'Boban', lastname: 'S', ssn: '123'}];
+
+// CRUD: Create Read Update Delete
+
+module.exports = {
+    index: (req, res) => {
+        res.render('index', { title: 'Balkan Express', subtitle: 'Hehehe', users: users });
+    },
+    hehe: (req, res) => {
+        res.render('hehe', { title: 'Hehe Express', subtitle: 'Hehe OG' });
+    },
+    viewUser: (req, res) => {
+        const user = users.find(user => user.id == req.params.id);
+
+        res.render('view-user', { user: user });
+    },
+    editUser: (req, res) => {
+        const user = users.find(user => user.id == req.params.id);
+
+        res.render('edit-user', { user: user });
+    },
+    create: (req, res) => {
+        res.render('create');
+    },
+    postCreate: (req, res) => {
+        const user = req.body;
+        user.id = users.length + 1;
+
+        users.push(user); // simulacija na povik za dodavanje zapis vo baza
+
+        res.redirect('/');
+    },
+    putEditUser: (req, res) => {
+        //Find index of specific object using findIndex method.    
+        
+        userIndex = users.findIndex((user => user.id == req.params.id));
+        users[userIndex] = req.body;
+
+        res.send({
+            success: true
+        });
+    },
+    viewUserforDelete: (req, res) => {
+        const user = users.find(user => user.id == req.params.id);
+
+        res.render('delete-user', { user: user });
+    },
+
+
+    deleteUser: (req, res) => { 
+        
+        userID = users.findIndex((user => user.id == req.params.id));
+        users[userID] = req.body; 
+        
+        users.splice(userID,1);
+        
+        res.send({
+            success: true
+        });
+
+
+    }
+}
---
+++ 03 classroom app/public/javascripts/index.js
@@ -0,0 +1,62 @@
+function deleteUser(userId) {
+    const form = document.getElementById('form');
+    
+
+    fetch(`http://localhost:3000/user/${userId}`, {
+        method: 'DELETE',
+        body: null,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    }).then(function (response) {
+        
+        if (response.ok) {
+            return response.json();
+        } else {
+            return Promise.reject(response);
+        }
+    }).then(function (data) {
+        
+        if (data.success) {
+            window.location.replace("http://localhost:3000/");
+        }
+    }).catch(function (err) {
+        
+        console.warn('Something went wrong.', err);
+    });
+};
+
+
+function updateUser(userId) {
+    const form = document.getElementById('form');
+    const user = {
+        id: userId,
+        name: form.name.value,
+        lastname: form.lastname.value,
+        email: form.email.value
+    };
+
+    fetch(`http://localhost:3000/user/${userId}`, {
+        method: 'PUT',
+        body: JSON.stringify(user),
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    }).then(function (response) {
+        // The API call was successful!
+        if (response.ok) {
+            return response.json();
+        } else {
+            return Promise.reject(response);
+        }
+    }).then(function (data) {
+        // This is the JSON from our response
+        if (data.success) {
+            window.location.replace("http://localhost:3000/");
+        }
+    }).catch(function (err) {
+        // There was an error
+        console.warn('Something went wrong.', err);
+    });
+}
+
